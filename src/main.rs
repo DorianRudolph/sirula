9f3a890ba1b04b36887d475ea6c4a469558c4972
Dorian Rudolph <10325838+DorianRudolph@users.noreply.github.com>
2020-08-03 01:47:03 +0200

    work on toolbar

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,10 @@ fn activate(application: &gtk::Application) {
         }
     });
 
+    window.connect_show(|w| {
+        println!("asdf");
+    });
+
     // window.set_size_request(1000, 500);
     // window.resize(1000, 500);
 
@@ -32,15 +36,43 @@ fn activate(application: &gtk::Application) {
     gtk_layer_shell::set_keyboard_interactivity(&window, true);
     gtk_layer_shell::set_layer(&window, gtk_layer_shell::Layer::Overlay);
     // gtk_layer_shell::auto_exclusive_zone_enable(&window);
+
+    // gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Left, 10);
+    // gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Right, 10);
+    // gtk_layer_shell::set_margin(&window, gtk_layer_shell::Edge::Top, 10);
+
     gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Left, true);
     gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Right, true);
     gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Top, true);
     gtk_layer_shell::set_anchor(&window, gtk_layer_shell::Edge::Bottom, false);
 
     // Set up a widget
-    let label = gtk::Label::new(Some(""));
-    label.set_markup("<span font_desc=\"20.0\">GTK Layer Shell example!</span>");
-    window.add(&label);
+    // let label = gtk::Label::new(Some(""));
+    // label.set_markup("<span font_desc=\"20.0\">GTK Layer Shell example!</span>");
+
+
+    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 10);
+    let entry = gtk::SearchEntry::new();
+    entry.set_property_width_request(300);
+
+    hbox.pack_start(&entry, false, false, 0);
+    
+    let scroll = gtk::ScrolledWindow::new(gtk::NONE_ADJUSTMENT, gtk::NONE_ADJUSTMENT);
+    scroll.set_property_vscrollbar_policy(gtk::PolicyType::Never);
+    let app_box = gtk::Box::new(gtk::Orientation::Horizontal, 10);
+
+    for i in 0..100 {
+        let icon = gtk::Image::new();
+        icon.set_from_icon_name(Some("ark"), gtk::IconSize::Dialog);
+        app_box.pack_end(&icon, false, false, 0);
+    }
+
+    scroll.add(&app_box);
+
+    hbox.pack_end(&scroll, true, true, 0);
+
+
+    window.add(&hbox);
     window.set_border_width(12);
     window.show_all()
 }
